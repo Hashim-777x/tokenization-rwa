fecd6c03be116fc9c9488b4aba49009274ab9ab3
Hashim-777x <syedhashim721@gmail.com>
2026-03-05 16:15:54 +0530

    deploy changes

--- script/DeployDTsla.sol
+++ script/DeployDTsla.sol
@@ -3,9 +3,19 @@ pragma solidity ^0.8.25;
 
 import {Script} from "forge-std/Script.sol";
 import {dTSLA} from "../src/dTSLA.sol";
+import {console2} from "forge-std/console2.sol";
 
 contract DeployDTsla {
     // Contract implementation
     string constant alpacaMintSource = "./functions/sources/alpacaBalance.js";
-    string constant alpacaRedeemSource = "./functions/sources/alpacaRedeem.js";
+    string constant alpacaRedeemSource = "";
+    uint256 constant subId = 6337;
+
+    function run() public {
+       string memory mintSource = vm.readFile(alpacaMintSource);
+
+        vm.startBroadcast();
+        dTSLA dtsla = new dTSLA(alpacaMintSource, subId, alpacaRedeemSource);
+        vm.stopBroadcast();
+    }
 }
